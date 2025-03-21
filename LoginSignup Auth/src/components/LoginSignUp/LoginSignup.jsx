ce0cbd170b7052a777237693cfc1eb1d8a3db441
Prince <oscarprince2020@gmail.com>
2025-03-21 14:02:47 -0700

    Third commit

--- LoginSignup Auth/src/components/LoginSignUp/LoginSignup.jsx
+++ LoginSignup Auth/src/components/LoginSignUp/LoginSignup.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginSignup.css";
 import email from "../Assets/email.png";
 import password from "../Assets/password.png";
 import profile from "../Assets/profile.svg";
 
 const LoginSignup = () => {
+
+    const [action, setAction] = useState("Sign Up");
+
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
         <div className="inputs"></div>
         <div className="input">
@@ -28,8 +31,8 @@ const LoginSignup = () => {
         Lost Password?<span>Click here!</span>
       </div>
       <div className="submit-container"></div>
-      <div className="submit">Sign Up</div>
-      <div className="submit">Log in</div>
+      <div className={action ==="Login"? "submit gray":"submit"}onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+      <div className={action==="Sign up "?"submit gray":"submit"}onClick={()=>{setAction("Log in")}}>Log in</div>
     </div>
   );
 };
